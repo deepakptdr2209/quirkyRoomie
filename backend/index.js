93d6cac96276c61f253a57e39bed658de79b5e2c
devdeepak <deepakptdr2209@gmail.com>
2025-02-18 18:32:14 +0530

    added updated indexjs file

--- backend/index.js
+++ backend/index.js
@@ -5,20 +5,17 @@ require('dotenv').config();
 
 
 
-// Create an asynchronous function to initialize the app
 const startServer = async () => {
     try {
-      // Wait for the database connection to be established
-      await connectDB; // This waits for the connection promise to resolve
+      await connectDB; 
   
-      // Once MongoDB is connected, start the server
       const PORT = process.env.PORT || 5000;
       app.listen(PORT, () => {
         console.log(`Server running on port ${PORT}`);
       });
     } catch (err) {
       console.error('Error connecting to MongoDB:', err);
-      process.exit(1); // Stop the process if MongoDB connection fails
+      process.exit(1); 
     }
     };
     startServer();
